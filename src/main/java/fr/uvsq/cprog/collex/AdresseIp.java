e309a63911b4a67bb2d46e68c98f290ad998f7a4
Elie-Chris-ElHaddad <elie-chris.el-haddad@ens.uvsq.fr>
2025-10-15 15:18:18 +0200

    nous avons exécuté les tests unitaires et modification de la classe AdresseIp

--- src/main/java/fr/uvsq/cprog/collex/AdresseIp.java
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIp.java
@@ -1,15 +1,43 @@
 package fr.uvsq.cprog.collex;
 
 public class AdresseIp {
-    private String ip;
+    private final String ip;
+
     public AdresseIp(String ip) {
+        if (ip == null || !ip.matches("^([0-9]{1,3}\\.){3}[0-9]{1,3}$")) {
+            throw new IllegalArgumentException("Adresse IP invalide : " + ip);
+        }
+
+        for (String part : ip.split("\\.")) {
+            int n = Integer.parseInt(part);
+            if (n < 0 || n > 255) {
+                throw new IllegalArgumentException("Octet invalide : " + part);
+            }
+        }
         this.ip = ip;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof AdresseIp))
+            return false;
+        AdresseIp other = (AdresseIp) o;
+        return ip.equals(other.ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return ip.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ip;
+    }
+
     public String getIp() {
         return ip;
     }
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
 }
-
--- src/test/java/fr/uvsq/cprog/collex/AdresseIPTest.java
+++ src/test/java/fr/uvsq/cprog/collex/AdresseIPTest.java
@@ -1,11 +1,35 @@
-package fr.uvsq.cprog.dns;
-
+package   fr.uvsq.cprog.collex;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AdresseIPTest {
-  @Test(expected = IllegalArgumentException.class)
-  public void rejectsBadIp() { new AdresseIP("999.1.2.3"); }
 
-  @Test
-  public void acceptsValidIp() { new AdresseIP("193.51.31.90"); }
+    @Test
+    public void testAdresseValide() {
+        AdresseIp ip = new AdresseIp("192.168.0.1");
+        assertEquals("192.168.0.1", ip.getIp());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdresseInvalideFormat() {
+        new AdresseIp("abc.def.ghi.jkl");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdresseInvalideValeur() {
+        new AdresseIp("999.10.10.10");
+    }
+
+    @Test
+    public void testEgalite() {
+        AdresseIp ip1 = new AdresseIp("10.0.0.1");
+        AdresseIp ip2 = new AdresseIp("10.0.0.1");
+        assertEquals(ip1, ip2);
+    }
+
+    @Test
+    public void testToString() {
+        AdresseIp ip = new AdresseIp("127.0.0.1");
+        assertEquals("127.0.0.1", ip.toString());
+    }
 }
